813cf5f4a60ed8d14e07d9a29d9ae6c97964866a
john <jayelwhy@yahoo.com>
2015-06-30 10:31:00 -0700

    include fullAddress function

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,13 @@ function Contact(firstName,lastName){
  Contact.prototype.fullName = function() {
    return this.firstName + " " + this.lastName;
  };
+
+ function Address(street, city, state) {
+   this.street = street;
+   this.city = city;
+   this.state = state;
+ };
+
+ Address.prototype.fullAddress = function() {
+   return this.street + ", " + this.city + ", " + this.state;
+ };
